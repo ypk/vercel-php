969d4230b75f0efb9135a8cb82fed225ad4343a4
ypk <yprasannakumar@gmail.com>
2022-03-09 18:42:17 +0000

    added missing files

---
+++ menu/head.php
@@ -0,0 +1,5 @@
+<script src="http://www.everestspicebristol.co.uk/bootstrap/js/bootstrap.js"></script>
+<link href="http://www.everestspicebristol.co.uk/bootstrap/css/bootstrap.css" rel="stylesheet" />
+<link href="http://www.everestspicebristol.co.uk/font-awesome/css/all.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Varela+Round&display=swap" rel="stylesheet"> 
+<link href="http://www.everestspicebristol.co.uk/css/style.css" rel="stylesheet" /></head>
--- menu/index.php
+++ menu/index.php
@@ -2346,12 +2346,6 @@
     <title>Our Menu - Everest Spice Bristol</title>
     <meta name="description" content=" ">
     <?php include('head.php'); ?>
-
-<script src="http://www.everestspicebristol.co.uk/bootstrap/js/bootstrap.js"></script>
-<link href="http://www.everestspicebristol.co.uk/bootstrap/css/bootstrap.css" rel="stylesheet" />
-<link href="http://www.everestspicebristol.co.uk/font-awesome/css/all.css" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Varela+Round&display=swap" rel="stylesheet"> 
-<link href="http://www.everestspicebristol.co.uk/css/style.css" rel="stylesheet" /></head>
 </head>
 
 <body>
