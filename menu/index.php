9f16f4d747bb565579ff8793534dccaec1f5c248
ypk <yprasannakumar@gmail.com>
2022-03-10 14:22:36 +0000

    debug borked php

--- menu/index.php
+++ menu/index.php
@@ -3,7 +3,7 @@ $page = basename($_SERVER['SCRIPT_NAME']);
 
 $column_count = 1;
 
-$menu_json = file_get_contents("menu.json");
+$menu_json = file_get_contents("./menu.json");
 $menu = json_decode($menu_json, true);
 
 echo $menu_json;
